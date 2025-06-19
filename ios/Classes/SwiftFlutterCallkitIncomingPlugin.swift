569cbef29d416994f0a8243c13cfe13bfa3ef2ce
trungnq <trungnguyen@aggregatoricapaci.com>
2025-06-19 10:10:40 +0700

    [fix][endCall][handle]

--- ios/Classes/SwiftFlutterCallkitIncomingPlugin.swift
+++ ios/Classes/SwiftFlutterCallkitIncomingPlugin.swift
@@ -573,7 +573,6 @@ public class SwiftFlutterCallkitIncomingPlugin: NSObject, FlutterPlugin, CXProvi
             }
         }else {
             self.answerCall = nil
-            self.shouldSendDeclineEvent = true
             sendEvent(SwiftFlutterCallkitIncomingPlugin.ACTION_CALL_ENDED, call.data.toJSON())
             if let appDelegate = UIApplication.shared.delegate as? CallkitIncomingAppDelegate {
                 appDelegate.onEnd(call, action)
@@ -581,6 +580,7 @@ public class SwiftFlutterCallkitIncomingPlugin: NSObject, FlutterPlugin, CXProvi
                 action.fulfill()
             }
         }
+        self.setEnableRejectAction(true)
     }
     
     
