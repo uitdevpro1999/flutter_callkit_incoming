725d3345fe9e58c11bdd3f3c383eecec7622f5ef
trungnq <trungnguyen@aggregatoricapaci.com>
2025-06-19 08:27:38 +0700

    [fix][endCall][handle]

--- ios/Classes/SwiftFlutterCallkitIncomingPlugin.swift
+++ ios/Classes/SwiftFlutterCallkitIncomingPlugin.swift
@@ -565,7 +565,7 @@ public class SwiftFlutterCallkitIncomingPlugin: NSObject, FlutterPlugin, CXProvi
         call.endCall()
         self.callManager.removeCall(call)
         if (self.answerCall == nil && self.outgoingCall == nil) {
-            if self.enableSendCallDeclineEvent {
+            if self.shouldSendDeclineEvent {
                 sendEvent(SwiftFlutterCallkitIncomingPlugin.ACTION_CALL_DECLINE, self.data?.toJSON())
             }
             if let appDelegate = UIApplication.shared.delegate as? CallkitIncomingAppDelegate {
